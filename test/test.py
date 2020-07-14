165ba09cab997ee13741d34ad066733eac116821
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2020-07-14 21:58:53 +0000

    renamed skeleton unit test

--- test/test.py
+++
@@ -1,3 +0,0 @@
-import nerdrage_controller_bridge
-
-import unittest
---
+++ test/test_bridge.py
@@ -0,0 +1,8 @@
+import nerdrage_controller_bridge
+
+import unittest
+
+class TestOne(unittest.TestCase):
+
+    def test_one(self):
+        self.assertTrue(True)
